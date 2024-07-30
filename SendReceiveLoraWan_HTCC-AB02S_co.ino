cd1a01fb299efd923c2ab95be3d9923d4aa436f6
frabbit97 <162334208+frabbit97@users.noreply.github.com>
2024-07-30 16:52:31 +0200

    Add files via upload

---
+++ SendReceiveLoraWan_HTCC-AB02S_co.ino
@@ -0,0 +1,141 @@
+/**
+ * This is an example of joining, sending and receiving data via LoRaWAN using a more minimal interface.
+ * 
+ * The example is configured for OTAA, set your keys into the variables below.
+ * 
+ * The example will upload a counter value periodically, and will print any downlink messages.
+ * 
+ * please disable AT_SUPPORT in tools menu
+ *
+ * David Brodrick.
+ */
+#include "LoRaWanMinimal_APP.h"
+#include "Arduino.h"
+
+/*
+ * set LoraWan_RGB to Active,the RGB active in loraWan
+ * RGB red means sending;
+ * RGB purple means joined done;
+ * RGB blue means RxWindow1;
+ * RGB yellow means RxWindow2;
+ * RGB green means received done;
+ */
+
+//Set these OTAA parameters to match your app/node in TTN
+uint8_t devEui[] = { 0x70, 0xb3, 0xd5, 0x7e, 0xD0, 0x06, 0x95, 0x50 };
+uint8_t appEui[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t appKey[] = { 0xc0, 0xA1, 0x29, 0x1f, 0x11, 0x60, 0xf7, 0xd7, 0x57, 0x00, 0xe9, 0x75, 0x37, 0x40, 0x35, 0xf4 };
+
+uint16_t userChannelsMask[6]={ 0x00FF,0x0000,0x0000,0x0000,0x0000,0x0000 };
+
+static uint8_t counter=0;
+
+float voltage;
+uint16_t level;
+#define coefficient_A 19.32
+#define coefficient_B -0.64
+
+///////////////////////////////////////////////////
+//Some utilities for going into low power mode
+TimerEvent_t sleepTimer;
+//Records whether our sleep/low power timer expired
+bool sleepTimerExpired;
+
+static void wakeUp()
+{
+  sleepTimerExpired=true;
+}
+
+static void lowPowerSleep(uint32_t sleeptime)
+{
+  sleepTimerExpired=false;
+  TimerInit( &sleepTimer, &wakeUp );
+  TimerSetValue( &sleepTimer, sleeptime );
+  TimerStart( &sleepTimer );
+  //Low power handler also gets interrupted by other timers
+  //So wait until our timer had expired
+  while (!sleepTimerExpired) lowPowerHandler();
+  TimerStop( &sleepTimer );
+}
+
+///////////////////////////////////////////////////
+void setup() {
+	Serial.begin(115200);
+
+  if (ACTIVE_REGION==LORAMAC_REGION_AU915) {
+    //TTN uses sub-band 2 in AU915
+    LoRaWAN.setSubBand2();
+  }
+ 
+  LoRaWAN.begin(LORAWAN_CLASS, ACTIVE_REGION);
+  
+  //Enable ADR
+  LoRaWAN.setAdaptiveDR(true);
+
+  while (1) {
+    Serial.print("Joining... ");
+    LoRaWAN.joinOTAA(appEui, appKey, devEui);
+    if (!LoRaWAN.isJoined()) {
+      //In this example we just loop until we're joined, but you could
+      //also go and start doing other things and try again later
+      Serial.println("JOIN FAILED! Sleeping for 30 seconds");
+      lowPowerSleep(30000);
+    } else {
+      Serial.println("JOINED");
+      break;
+    }
+  }
+}
+
+///////////////////////////////////////////////////
+void loop()
+{
+  //Counter is just some dummy data we send for the example
+  counter++; 
+  
+  //In this demo we use a timer to go into low power mode to kill some time.
+  //You might be collecting data or doing something more interesting instead.
+  lowPowerSleep(15000);  
+  //return adc level 0-4095;
+  level=analogRead(ADC); 
+
+  /*return the voltage in mV
+  * for asr650x, max value can be read is 2400mV
+  * for asr6601,value can be read is 100mV-1100mV
+  */
+  voltage=analogReadmV(ADC);
+  //formula presa dalla libreria MQ7
+  float v_in=2.35;
+  float v_out=voltage/1000;
+  float ratio=(v_in-v_out)/v_out;
+  float ppm=coefficient_A*pow(ratio,coefficient_B);
+  Serial.println("Sending data via loraWan");
+  Serial.print("voltage: ");
+  Serial.print(voltage);
+  Serial.printf(",  level: ");
+  Serial.print(level);
+  Serial.printf(",  ppm: ");
+  Serial.println(ppm);
+  uint8_t value=(uint8_t)ppm;
+  char data[21];
+  // Usa snprintf per formattare la stringa con 2 decimali
+  snprintf(data, sizeof(data), "V%.2fL%dP%.2f",voltage, (int)level, ppm);
+  //Here we send confirmed packed (ACK requested) only for the first five (remember there is a fair use policy)
+  bool requestack=counter<5?true:false;
+  if (LoRaWAN.send(strlen(data), (uint8_t *)data, 1, requestack)) {
+    Serial.println("Send OK");
+  } else {
+    Serial.println("Send FAILED");
+  }
+}
+
+///////////////////////////////////////////////////
+//Example of handling downlink data
+void downLinkDataHandle(McpsIndication_t *mcpsIndication)
+{
+  Serial.printf("Received downlink: %s, RXSIZE %d, PORT %d, DATA: ",mcpsIndication->RxSlot?"RXWIN2":"RXWIN1",mcpsIndication->BufferSize,mcpsIndication->Port);
+  for(uint8_t i=0;i<mcpsIndication->BufferSize;i++) {
+    Serial.printf("%02X",mcpsIndication->Buffer[i]);
+  }
+  Serial.println();
+}
